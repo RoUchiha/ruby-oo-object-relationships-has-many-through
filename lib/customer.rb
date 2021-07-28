9aa48011600c3f532ebfc5852750de8b6744e8a7
RoUchiha <swaggsikh@gmail.com>
2021-07-28 15:56:02 -0500

    Done.

--- lib/customer.rb
+++ lib/customer.rb
@@ -1,3 +1,42 @@
 class Customer
   
+    attr_accessor :name, :age
+
+    @@all = []
+
+    def initialize(name, age)
+        @name = name
+        @age = age
+        @@all << self
+    end
+
+    def self.all
+        @@all
+    end
+
+    def new_meal(waiter, total, tip)
+        Meal.new(waiter, self, total, tip)
+    end
+
+    def meals
+        all_meals = []
+        Meal.all.each do |m|
+            if m.customer == self
+                all_meals << m
+            end
+        end
+        return all_meals
+    end
+
+    def waiters
+        all_waiters = []
+        Meal.all.each do |m|
+            if m.customer == self
+                all_waiters << m.waiter
+            end
+        end
+        return all_waiters
+    end
+
+
 end
